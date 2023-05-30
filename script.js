40f6aa2eab26a140983edcb43c22111d29bd8e56
Nicolas Fuentes <nfuentes11@gmail.com>
2023-05-30 16:28:33 -0400

    Modals done for login and sing up

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const mouseEfect = document.querySelectorAll("logo-move");
 
-setInterval(() => {
-  document.querySelectorAll("h1").onmouseover = (event) => {
-    event.target.innertext = event.target.innertext
-      .split("")
-      .map((letter) => letters[Math.floor(Math.random() * 26)])
-      .join("");
-  };
-}, 30);
